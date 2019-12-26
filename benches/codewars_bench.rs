5affd335fa124a63a78f7e60b76201b55b41da14
Dmitriy Holkin <alukard.develop@gmail.com>
2019-12-27 00:36:14 +0400

    some criterion benchmarks

--- benches/codewars_bench.rs
+++ benches/codewars_bench.rs
@@ -1,9 +1,39 @@
 use codewars::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Duration;
+use codewars::integers_recreating_one::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("sum_by_factors", |b| {
-        b.iter(|| sum_by_factors::sum_of_divided(black_box(vec![15, 21, 24, 30, -45])))
+    let mut group = c.benchmark_group("integers_recreating_one");
+    group.sample_size(10);
+    group.measurement_time(Duration::new(60, 0));
+
+    let mut divisors = Divisors::with_capacity(200);
+    let mut primes = Divisors::with_capacity(100);
+    // let mut primes = Primes::with_capacity(100);
+    // group.bench_function("list_squares", |b| {
+    //     b.iter(|| {
+    //         list_squared(black_box(1), black_box(200000));
+    //     })
+    // });
+    group.bench_function("get_divisors", |b| {
+        b.iter(|| {
+            let range = black_box(1..200000);
+            range.for_each(|num| {
+                // trial_prime(num, &mut primes);
+                // get_divisors(0, 1, &mut primes);
+                get_divisors_old(num, &mut primes, &mut divisors);
+            })
+        })
+    });
+    group.bench_function("trial_prime", |b| {
+        b.iter(|| {
+            let range = black_box(1..200000);
+            range.for_each(|num| {
+                // trial_prime(num, &mut primes);
+                trial_prime_old(num, &mut primes);
+            })
+        })
     });
 }
 
